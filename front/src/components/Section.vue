ffdf3452fce4018b9bf2d4d52865edca33644931
gnemek20 <gnemek20@gmail.com>
2023-08-01 06:38:41 +0900

    커뮤니티에 이미지 업로드 첨부파일 추가

--- front/src/components/Section.vue
+++ front/src/components/Section.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="section">
+  <div class="section flex flex-column">
     <slot></slot>
   </div>
 </template>
--- front/src/views/Community.vue
+++ front/src/views/Community.vue
@@ -4,20 +4,41 @@
       <div class="title flex">
         <h2>SEOUL 3.0</h2>
       </div>
-      <hr>
     </div>
-    <div class="content flex justify-center full-width">
+    <div class="content flex flex-column align-center full-width">
       <div class="area flex flex-column">
         <SectionVue>
-          <div class="tool flex">
-            <button>upload</button>
+          <div class="uploadArea flex justify-center" ref="uploadArea">
+            <button ref="uploadButton" @click="clickedUploadButton">upload</button>
+            <div class="uploadInput flex justify-center" ref="uploadInput">
+              <div class="flex full-width full-height">
+                <label class="flex justify-center align-center full-width full-height pointer" for="inputImage">
+                  <h3>이미지 업로드</h3>
+                </label>
+                <input id="inputImage" class="none" type="file" accept="image/*" multiple>
+              </div>
+            </div>
           </div>
         </SectionVue>
         <hr>
         <SectionVue>
-          <div>
-            <p>chicks</p>
+          <div class="flex justify-center">
+            <p>당신의 멋진 모습을 보여주세요!</p>
           </div>
+        </SectionVue>
+      </div>
+      <div class="area flex flex-column">
+        <SectionVue>
+          <img src="@/assets/ddp01.png">
+        </SectionVue>
+        <hr>
+        <SectionVue>
+          <img src="@/assets/ddp02.png">
+        </SectionVue>
+      </div>
+      <div class="area flex flex-column">
+        <SectionVue>
+          <img src="@/assets/ddp03.png">
         </SectionVue>
       </div>
     </div>
@@ -30,6 +51,14 @@ import SectionVue from '@/components/Section.vue';
 export default {
   components: {
     SectionVue
+  },
+  methods: {
+    clickedUploadButton() {
+      this.$refs['uploadButton'].className = 'none';
+      this.$refs['uploadInput'].style.width = '100%';
+      this.$refs['uploadInput'].style.height = '100px';
+      this.$refs['uploadArea'].style.height = '100%';
+    }
   }
 }
 </script>
@@ -43,10 +72,14 @@ hr {
   margin: 0 20px;
 }
 
+img {
+  border-radius: 5px;
+}
+
 .header {
   top: 0;
   background-color: white;
-  box-shadow: 0px 0px 2px 0px;
+  box-shadow: 0px 0px 3px 0px;
 }
 
 .header > .title {
@@ -60,19 +93,32 @@ hr {
 }
 
 .content {
-  margin-top: 38px;
+  margin: 38px 0;
 }
 
 .content > .area {
-  width: 500px;
+  width: 700px;
   border-radius: 5px;
-  border-bottom-left-radius: 0px;
-  border-bottom-right-radius: 0px;
+  /* border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px; */
   margin-top: 80px;
   background-color: white;
 }
 
-.tool > button {
-  margin-left: auto;
+.uploadArea {
+  height: 27.6px;
+  transition: all 1s;
+}
+
+.uploadInput {
+  width: 0px;
+  height: 0px;
+  overflow: hidden;
+  border: 5px dashed white;
+  transition: all 1s;
+}
+
+.uploadInput:hover {
+  border: 5px dashed dimgray;
 }
 </style>
