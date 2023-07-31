45d3f02dd5aef755c3ac95ea6a5be13b11c49bb5
gnemek20 <gnemek20@gmail.com>
2023-07-31 14:32:42 +0900

    vue 개발 중단

--- src/views/code.vue
+++ src/views/code.vue
@@ -3,3 +3,33 @@
     <p>hello world!</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    const locationHref = document.location.href;
+    const img = document.querySelectorAll('img');
+
+    window.alert('check');
+    console.log(locationHref);
+
+    if (img !== null) {
+      img.forEach((img, i) => {
+        console.log(`${i}: ${img}`);
+      })
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
